1e8f62be75f0cf99779734b75161309448e10644
David Ko and Jessica Armstrong <pair+david+jessica@xogrp.com>
2016-08-17 15:27:22 -0400

    Replace initially loaded tweets unless 'minority' is in the text

--- myScript.js
+++ myScript.js
@@ -1,8 +1,15 @@
 var newTweet = "Pew Study finds race conversations noticeably absent from white people's timelines http://bit.ly/2b18nFy";
 
 var tweets = document.getElementsByClassName("tweet-text");
-var firstTweet = tweets[0];
 
-firstTweet.textContent = newTweet;
+for(var i = 0; i < 40; i++){
+  if(tweetOnRace(tweets[i].textContent)){
+    tweets[i].textContent = newTweet;
+  }
+}
 
-console.log(firstTweet.textContent);
+function tweetOnRace(text) {
+  if (text.indexOf('minority') === -1){
+    return true;
+  }
+}
